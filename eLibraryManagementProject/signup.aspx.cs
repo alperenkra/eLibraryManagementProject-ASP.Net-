a784bb5e9fd23e914b4ec1dd2f164622f96969a0
alperenkra <alperenkara055@gmail.com>
2024-05-09 18:43:11 +0300

    registration of users with the same id was avoided

--- eLibraryManagementProject/signup.aspx.cs
+++ eLibraryManagementProject/signup.aspx.cs
@@ -24,6 +24,15 @@ namespace eLibraryManagementProject
         protected void Button1_Click(object sender, EventArgs e)
         {
             SignUpNewUser();
+
+            if (checkMemberExists())
+            {
+                Response.Write("<script>alert('Member Already Exist with this Member ID, try another ID');</script>");
+            }
+            else
+            {
+                SignUpNewUser();
+            }
         }
 
         //user defined method
@@ -31,6 +40,37 @@ namespace eLibraryManagementProject
         bool checkMemberExists()
         {
 
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from member_master_tbl where member_id='"+TextBox2.Text.Trim()+"';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                    return false;
+
+              
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+
+            
         }
 
         void SignUpNewUser()
